9f895aec0dd548ae03beaf8f174949b8ec8c8ea6
Aditya Garg <sutble@bitbucket.org>
2016-12-21 23:35:44 -0800

    very basic search feature implemented

--- Nutrition/ViewController.swift
+++ Nutrition/ViewController.swift
@@ -8,17 +8,60 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    var foodArray = ["Chole Rice", "Chicken with Rice/Vegetables", "Turkey Sandwich"]
+    var filteredArray = [String]()
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        searchBarSetup()
+    }
+    
+    //MARK: - Search Bar
+    
+    func searchBarSetup(){
+        let searchBar = UISearchBar(frame: CGRect(x: 0, y: 0, width: UIScreen.main.bounds.width, height: 70))
+        searchBar.showsScopeBar = true
+        searchBar.scopeButtonTitles = ["protein","carbs","fat"]
+        searchBar.delegate = self
+        searchBar.selectedScopeButtonIndex = 0
+        tableView.tableHeaderView = searchBar
+    }
+    
+    func searchBar(_ searchBar: UISearchBar, textDidChange searchText: String) {
+        
+        if(searchText.isEmpty){
+            foodArray = ["Chole Rice", "Chicken with Rice/Vegetables", "Turkey Sandwich"]
+        }
+        
+        foodArray = foodArray.filter { foodItem in
+            return foodItem.lowercased().contains(searchText.lowercased())
+        }
+        tableView.reloadData()
+    }
+    
+    //MARK: - Table View
+
+    public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return foodArray.count
+    }
+    
+    
+    
+    public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    
+    {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "cell")
+        cell.textLabel?.text = foodArray[indexPath.row]
+        return cell
+        
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
